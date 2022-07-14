8ab08ec08ccebf0bf5bb5df0d52b745deaeee431
Robert Hoekstra <r.hoekstra@arcady.nl>
2022-07-14 18:03:50 +0200

    import headerimage

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from "@/components/TheWelcome.vue";
+import HeaderImage from "@/components/HeaderImage.vue";
+</script>
+
+<template>
+  <main>
+    <HeaderImage />
+    <TheWelcome />
+  </main>
+</template>
